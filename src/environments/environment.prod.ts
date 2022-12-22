4704fef6f2dd2b3b5197cb7a772f46d33682d2c1
fernandezmayra <fernandezmayra239@gmail.com>
2022-12-22 00:25:17 -0300

    modificación environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   firebase: {
-    projectId: 'mcfsfrontend',
-    appId: '1:9438718235:web:7ec2e753314658ad600bbe',
-    storageBucket: 'mcfsfrontend.appspot.com',
-    locationId: 'southamerica-east1',
-    apiKey: 'AIzaSyAqNC6Wq3K0akW36JMtEVzc5ItEUJO14BE',
-    authDomain: 'mcfsfrontend.firebaseapp.com',
-    messagingSenderId: '9438718235',
-    measurementId: 'G-BX8E0SJJQC',
+    projectId: 'miportfolio-1',
+    appId: '1:202050251922:web:34c59ecba0ce63ffc94a4b',
+    storageBucket: 'miportfolio-1.appspot.com',
+    apiKey: 'AIzaSyCSX2YmCF3T7spL_XQY8L8DIeI6WThQNJ0',
+    authDomain: 'miportfolio-1.firebaseapp.com',
+    messagingSenderId: '202050251922',
+    measurementId: 'G-LEP1KXWN4R',
+    locationId: 'nam5 (us-central)'
   },
   production: true
 };
