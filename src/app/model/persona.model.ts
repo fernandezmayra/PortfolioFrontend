67e4c05924179fd8a23dc097bbd7fbe7c179bd21
fernandezmayra <fernandezmayra239@gmail.com>
2023-02-03 23:40:30 -0300

    cambio persona

--- src/app/model/persona.model.ts
+++ src/app/model/persona.model.ts
@@ -7,11 +7,11 @@ export class persona{
     titulo: string;
     email: string;
     imgBackground: string;
-    github: string;
+    twitter: string;
     facebook: string;
     instagram: string;
 
-    constructor(nombre: string, apellido: string, descripcion: string, img: string, titulo: string, email: string, imgBackground: string, github: string, facebook: string, instagram: string){
+    constructor(nombre: string, apellido: string, descripcion: string, img: string, titulo: string, email: string, imgBackground: string, twitter: string, facebook: string, instagram: string){
         this.nombre = nombre;
         this.apellido = apellido;
         this.descripcion = descripcion;
@@ -19,7 +19,7 @@ export class persona{
         this.titulo = titulo;
         this.email = email;
         this.imgBackground = imgBackground;
-        this.github = github;
+        this.twitter = github;
         this.facebook = facebook;
         this.instagram = instagram;
     }
